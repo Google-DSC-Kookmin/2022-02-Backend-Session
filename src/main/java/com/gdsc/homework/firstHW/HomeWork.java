211765dde338ed24913765a9777ee4281b932238
김동원 <dongwon0103@naver.com>
2022-10-16 15:21:53 +0900

    refactor: 첫 번째 과제 Homework -> HomeworkController 이름 변경 및 수정

--- src/main/java/com/gdsc/homework/firstHW/HomeWork.java
+++
@@ -1,8 +0,0 @@
-package com.gdsc.homework.firstHW;
-
-public class HomeWork {
-    public static void main(String[] args) {
-        GdscMember gdscMember = new GdscMember();
-        gdscMember.playCoding();
-    }
-}
---
+++ src/main/java/com/gdsc/homework/firstHW/controller/HomeWorkController.java
@@ -0,0 +1,19 @@
+package com.gdsc.homework.firstHW.controller;
+
+import com.gdsc.homework.firstHW.domain.GdscMember;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeWorkController {
+    private final GdscMember gdscMember;
+
+    public HomeWorkController(GdscMember gdscMember) {
+        this.gdscMember = gdscMember;
+    }
+
+    @RequestMapping("/homework")
+    public String homeSork() {
+        return gdscMember.playCoding();
+    }
+}

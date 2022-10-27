1e92fc925b622763186dd5ab0dddc1bc88069fed
김진재 <jinjae.dev@gmail.com>
2022-10-27 14:28:15 +0900

    refactor: 첫 번째 과제 GdscMember 수정

--- src/main/java/com/gdsc/homework/firstHW/GdscMember.java
+++ src/main/java/com/gdsc/homework/firstHW/GdscMember.java
@@ -1,14 +1,17 @@
 package com.gdsc.homework.firstHW;
 
-public class GdscMember {
-    private Laptop laptop;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
 
-    public GdscMember() {
-        this.laptop = new Laptop();
+@Component
+public class GdscMember {
+    private final Computer computer;
+
+    public GdscMember(@Qualifier("Laptop") Computer computer) {
+        this.computer = computer;
     }
 
-    public void playCoding(){
-        System.out.println("서버 파트원이 코딩을 하고 있어요!");
-        this.laptop.coding();
+    public String playCoding() {
+        return("서버 파트원이 코딩을 하고 있어요!<br>" + this.computer.Coding());
     }
 }

be6c229728763774cce69ec057c9de181d0d7cc0
LeeJejune <leejejune0625@gmail.com>
2022-10-31 13:26:15 +0900

    feat: 더 쉬운 예제 추가

--- src/main/java/com/gdsc/homework/thridstudy/controller/member/MemberController.java
+++ src/main/java/com/gdsc/homework/thridstudy/controller/member/MemberController.java
@@ -1,21 +1,59 @@
 package com.gdsc.homework.thridstudy.controller.member;
 
 import com.gdsc.homework.thridstudy.controller.member.dto.requset.MemberRequest;
+import com.gdsc.homework.thridstudy.service.member.dto.response.MemberResponse;
 import com.gdsc.homework.thridstudy.service.member.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
 import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
 public class MemberController {
+    private final Logger log = LoggerFactory.getLogger(getClass());
 
     private final MemberService memberService;
 
-    @PostMapping("/member/new")
+    @PostMapping("/request-body")
+    public MemberRequest requestBody(@RequestBody MemberRequest helloData){
+        log.info("helloDate = {}", helloData.toString());
+        return helloData;
+    }
+
+    @GetMapping("/request-param-v3")
+    public String requestParam(@RequestParam String username, @RequestParam int age) {
+        log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+
+    @GetMapping("/mapping/users/{userId}/orders/{orderId}")
+    public String mappingPath(@PathVariable String userId, @PathVariable String orderId) {
+        log.info("mappingPath userId={}, orderId={}", userId, orderId);
+        return "ok";
+    }
+
+
+
+    @PostMapping(value = "/member/new", consumes = "application/json")
+//    @RequestMapping(value = "/member/new", method = RequestMethod.POST)
     public void saveMember(@Valid @RequestBody final MemberRequest request){
         memberService.signUp(request.toServiceDto());
+    }
+    @GetMapping("/member/{userId}")
+    public MemberResponse getMember(@PathVariable Long userId){
+        return memberService.getMember(userId);
+    }
+
+    @GetMapping("/member")
+    public MemberResponse getMemberByName(@RequestParam(required = true) String name){
+        return memberService.getMemberByName(name);
     }
 }

36788193549e2a3b5e5331d70c7fffd155b4d11b
EastWon0103 <dongwon000103@gmail.com>
2022-11-11 01:04:18 +0900

    add: @ToString 어노테이션 추가

--- src/main/java/com/gdsc/homework/HW3/controller/dto/request/MemberRequest.java
+++ src/main/java/com/gdsc/homework/HW3/controller/dto/request/MemberRequest.java
@@ -1,12 +1,10 @@
 package com.gdsc.homework.HW3.controller.dto.request;
 
 import com.gdsc.homework.HW3.service.dto.request.MemberServiceRequest;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Getter
+@ToString
 @NoArgsConstructor(access = AccessLevel.PRIVATE) // 빈 생성자가 없으면 에러나는 듯
 @AllArgsConstructor
 public class MemberRequest {

bd68053c79e37228625c83895313b6ba9a708fc1
solhee-hwang <solhee0912@kookmin.ac.kr>
2022-11-10 19:14:23 +0900

    feat : MemberRequest 추가

--- src/main/java/com/gdsc/homework/SecondHW/controller/member/dto/MemberRequest.java
+++ src/main/java/com/gdsc/homework/SecondHW/controller/member/dto/MemberRequest.java
@@ -1,7 +1,13 @@
 package com.gdsc.homework.SecondHW.controller.member.dto;
 
+import lombok.*;
+@ToString
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class MemberRequest {
-
-
+    private String userId;
+    private String name;
+    private String description;
 
 }

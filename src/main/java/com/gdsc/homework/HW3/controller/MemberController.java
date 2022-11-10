2238f0838a2a22b346b88d717c1593faf758ac67
EastWon0103 <dongwon000103@gmail.com>
2022-11-11 01:52:42 +0900

    add: log와 search 기능 추가

--- src/main/java/com/gdsc/homework/HW3/controller/MemberController.java
+++ src/main/java/com/gdsc/homework/HW3/controller/MemberController.java
@@ -7,10 +7,9 @@ import com.gdsc.homework.HW3.service.dto.response.MemberServiceResponse;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,17 +18,36 @@ public class MemberController {
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
     private final MemberService memberService;
 
+    // 단순히 로그 찍는 기능
     @GetMapping("/log")
-    public void log() {
+    public final String log() {
         logger.info("GDSC Backend");
+        return "LOG SUCCESS!";
+    }
+    // request param을 이용하여 로그 찍기
+    @GetMapping("/log/{userId}")
+    public final String userIdLog(@PathVariable String userId) {
+        logger.info("USER ID: {}", userId);
+        return "LOG SUCCESS!";
     }
 
-    @GetMapping("/log/{id}")
-
-
+    // requestBody를 통해 signUp +memberRequest log 추가
     @PostMapping(value = "/signUp", consumes = "application/json")
-    public MemberResponse signUp(@RequestBody MemberRequest memberRequest) {
+    public final MemberResponse signUp(@RequestBody MemberRequest memberRequest) {
+        logger.info(memberRequest.toString());
         MemberServiceResponse response = memberService.enroll(memberRequest.toServiceDto(memberRequest));
         return MemberResponse.newInstance(response.getId(), response.getName(), response.getUserId(), response.getDescription());
+    }
+
+    @GetMapping("/search")
+    public final MemberResponse search(@RequestParam String userId) {
+        logger.info("USER ID: {}", userId);
+        MemberServiceResponse response = memberService.search(userId);
+        logger.info("Search Success {}", response.toString());
+        return MemberResponse.newInstance(
+                    response.getId(),
+                    response.getName(),
+                    response.getUserId(),
+                    response.getDescription());
     }
 }

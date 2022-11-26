33ededc4bc50b68a9ea3bdaf18499fca00b4fd51
EastWon0103 <dongwon000103@gmail.com>
2022-11-27 00:28:15 +0900

    feat: Comment domain 객체 생성

---
+++ src/main/java/com/gdsc/homework/HW5/domain/Comment.java
@@ -0,0 +1,29 @@
+package com.gdsc.homework.HW5.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name="article_id")
+    private Article article;
+    @ManyToOne
+    @JoinColumn(name="user_id")
+    private User user;
+    private String content;
+
+    private Comment(Article article, User user, String content) {
+        this.article = article;
+        this.user = user;
+        this.content = content;
+    }
+}

1a8ccf67e2c3b750831bbd8fddd2045a083667cd
SuperH0ng <ghdtmdgus100@gmail.com>
2022-11-28 00:56:56 +0900

    feat : scanBasePackages 설정

--- src/main/java/com/gdsc/homework/HomeworkApplication.java
+++ src/main/java/com/gdsc/homework/HomeworkApplication.java
@@ -3,7 +3,7 @@ package com.gdsc.homework;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages= {"com.gdsc.homework.fifthHw"})
 public class HomeworkApplication {
 
 	public static void main(String[] args) {

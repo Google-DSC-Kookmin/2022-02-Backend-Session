b0cec5f442846e94dfad194cf439b6d8f40ca94b
LeeJejune <leejejune0625@gmail.com>
2022-12-13 19:48:12 +0900

    fix: 프로젝트 초기화

--- src/main/java/com/gdsc/homework/thirdstudy/domain/member/MemberRepository.java
+++
@@ -1,9 +0,0 @@
-package com.gdsc.homework.thirdstudy.domain.member;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface MemberRepository extends JpaRepository<Member, Long> {
-    Optional<Member> findMemberByName(String name);
-}

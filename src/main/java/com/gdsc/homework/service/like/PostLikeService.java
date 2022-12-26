2e794e6b20ec2b4131a5830b47842fbd5c96196a
EastWon0103 <dongwon000103@gmail.com>
2022-12-26 23:23:21 +0900

    add: PostLike에 validate 추가 설정

--- src/main/java/com/gdsc/homework/service/like/PostLikeService.java
+++ src/main/java/com/gdsc/homework/service/like/PostLikeService.java
@@ -6,12 +6,11 @@ import com.gdsc.homework.domain.post.Post;
 import com.gdsc.homework.domain.post.PostRepository;
 import com.gdsc.homework.domain.user.User;
 import com.gdsc.homework.domain.user.UserRepository;
+import com.gdsc.homework.service.like.dto.request.DeletePostLikeServiceRequest;
 import com.gdsc.homework.service.like.dto.request.PostLikeServiceRequest;
 import com.gdsc.homework.validAPI.PostValidation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -32,14 +31,16 @@ public class PostLikeService {
         return postLike.getId();
     }
 
-    public final void deleteLike(Long postLikeId) {
-        validatePostLike(postLikeId);
-        postLikeRepository.deleteById(postLikeId);
+    public final void deleteLike(DeletePostLikeServiceRequest deletePostLikeServiceRequest) {
+        validatePostLike(deletePostLikeServiceRequest.getPostLikeId());
+        PostLike postLike = postLikeRepository.findById(deletePostLikeServiceRequest.getPostLikeId()).get();
+        validatePostLikeWithUser(deletePostLikeServiceRequest.getEmail(), postLike);
+        postLikeRepository.deleteById(deletePostLikeServiceRequest.getPostLikeId());
     }
     // postlike 서비스 한 부분에서만 사용할 것 같기 때문에 따로 valid한지 구현하지 않음
     private void validatePostLike(Long postLikeId) {
         if(postLikeRepository.findById(postLikeId).isEmpty()){
-            throw new IllegalArgumentException("post id가 존재하지 않음");
+            throw new IllegalArgumentException("postlike id가 존재하지 않음");
         }
     }
 
@@ -47,5 +48,11 @@ public class PostLikeService {
         postLikeRepository.findByUserAndPost(user, post).ifPresent(
                 postLike -> {throw new IllegalArgumentException("이미 좋아요를 눌렀습니다.");}
         );
+    }
+
+    private void validatePostLikeWithUser(String email, PostLike postLike) {
+        if(!postLike.getUser().getEmail().equals(email)) {
+            throw new IllegalArgumentException("해당 유저가 좋아요(id)를 누르지 않았습니다.");
+        }
     }
 }
